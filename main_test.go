c87ff9229e4479f74521ab81ed1ee88e31eb1f64
Глазков Андрей Васильевич <AVGlazkov@alfabank.ru>
2023-12-15 02:26:10 +0300

    add tests

---
+++ main_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainHandlerWhenCorrectRequest(t *testing.T) {
+	//GIVEN
+	count := 2
+	city := "moscow"
+	url := fmt.Sprintf("/cafe?count=%d&city=%s", count, city)
+	req := httptest.NewRequest("GET", url, nil)
+
+	expectedBody := "Мир кофе,Сладкоежка"
+
+	//WHEN
+	responseRecorder := serveHttpRequest(req)
+	actualBody := string(responseRecorder.Body.Bytes()[:])
+
+	//THEN
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Equal(t, expectedBody, actualBody)
+
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	//GIVEN
+	count := 5
+	city := "moscow"
+	url := fmt.Sprintf("/cafe?count=%d&city=%s", count, city)
+	req := httptest.NewRequest("GET", url, nil)
+
+	expectedBody := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
+
+	//WHEN
+	responseRecorder := serveHttpRequest(req)
+	actualBody := string(responseRecorder.Body.Bytes()[:])
+
+	//THEN
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Equal(t, expectedBody, actualBody)
+
+}
+
+func TestMainHandlerWhenUnsupportedCity(t *testing.T) {
+	//GIVEN
+	count := 2
+	city := "nsk"
+	url := fmt.Sprintf("/cafe?count=%d&city=%s", count, city)
+	req := httptest.NewRequest("GET", url, nil)
+
+	expectedBody := "wrong city value"
+
+	//WHEN
+	responseRecorder := serveHttpRequest(req)
+	actualBody := string(responseRecorder.Body.Bytes()[:])
+
+	//THEN
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+	assert.Equal(t, expectedBody, actualBody)
+
+}
+
+func serveHttpRequest(request *http.Request) *httptest.ResponseRecorder {
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, request)
+	return responseRecorder
+}
